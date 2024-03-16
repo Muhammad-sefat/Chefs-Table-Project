9a60787dbcd4ef0607dfd3411bc6a91f907956a9
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-03-16 12:17:41 +0600

    Add First Card

--- src/Components/Food/Food.jsx
+++ src/Components/Food/Food.jsx
@@ -1,24 +1,37 @@
 const Food = ({ food }) => {
   return (
-    <div>
-      <div className="grid md:grid-cols-2 gap-5">
-        <div className="card bg-base-100 shadow-xl rounded-xl">
+    <div className="flex justify-between">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:w-[65%]">
+        <div className="card bg-base-100 shadow-xl pt-5">
           <figure>
             <img
+              className="rounded-lg"
               src={
                 "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
               }
               alt="Shoes"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
+          <div className="text-start p-5">
+            <h2>name</h2>
+            <p>title</p>
+            <hr />
+            <h3>Ingredients 6</h3>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <div className="flex justify-between items-center">
+              <div>
+                <h2>minutes</h2>
+              </div>
+              <div>calories</div>
             </div>
+            <button>Want To Cook</button>
           </div>
         </div>
+      </div>
+      <div className="md:w-[30%]">
+        <h2>Want to cook</h2>
       </div>
     </div>
   );
