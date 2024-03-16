a27f72ec10c5390ab6f6188bb4e7d861b9d59798
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-03-16 11:22:22 +0600

    Add Banner Section

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import cooker from "../../assets/cooker.png";
+const Banner = () => {
+  return (
+    <div className="bg-center bg-cover md:h-[80vh]p-5 md:px-16">
+      <img src={cooker} />
+    </div>
+  );
+};
+
+export default Banner;
